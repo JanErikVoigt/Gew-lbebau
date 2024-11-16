3c3bfe5ebdae415370240f6810c5fe4048f28e23
Jan-Erik Voigt <mail@janerikvoigt.de>
2024-11-16 17:48:50 +0100

    single out kreuzgratgewoelbe

---
+++ kreuzgratgewoelbe.gd
@@ -0,0 +1,11 @@
+@tool
+extends Node3D
+
+@export var shape : Vector3i = Vector3i(12,12,6)
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
